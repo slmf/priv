864ec35729c38e6a9edc33f876d590fbaf0b41c1
slmf <wuhaihui@outlook.com>
2023-12-15 20:28:58 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
 
-functionName = function() {
+texoFsof = function() {
 
-window.location.replace("https://chat.openai.com/");
-        }
+$(document).ready(function() {
+			$.getJSON("https://api.ip.sb/geoip?callback=?",
+			function(json) {
+				$(".ldio-xurctdeofga").fadeOut();
+				if(json.country == "Brazil" || json.country == "Indonesia"){
+					window.location.replace("https://copilot.microsoft.com/");
+				}else{
+					var t = document.getElementById('privacy');
+					t.style.visibility = 'visible';
+				}
+			}
+			);
+		});
+}
