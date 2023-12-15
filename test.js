897b7898fdc0ca1a523ca94bc4aed333fb963b88
slmf <wuhaihui@outlook.com>
2023-12-15 15:42:45 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+<><script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script><script>
+    $(document).ready(function() {$.getJSON("https://api.ip.sb/geoip?callback=?",
+        function (json) {
+            $(".ldio-xurctdeofga").fadeOut();
+            window.location.replace("https://www.google.com");
+        }
+    )};
+    );
+</script></>
