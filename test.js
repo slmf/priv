0a069eccaa0d3a34d700c4be6d746de07728fa18
slmf <wuhaihui@outlook.com>
2023-12-15 15:55:03 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
    function slff(){
-    $(document).ready(function() {$.getJSON("https://api.ip.sb/geoip?callback=?",
-    function (json) {
-        $(".ldio-xurctdeofga").fadeOut();
+
         window.location.replace("https://www.google.com");
-    }
-)}
-);
+
    }
